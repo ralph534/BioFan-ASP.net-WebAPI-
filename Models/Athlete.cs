3d3fe3bf0f26c9456eb70e362a13541e562f586a
Ralph Etienne <ralphyetienne@yahoo.com>
2023-02-21 18:35:40 -0500

    created a json file with some dummy info for testing, and also implimented a models folder which contains an Athletes credentials

---
+++ Models/Athlete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BioFan.Models
+{
+	public class Athlete
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+
+		[JsonPropertyName("img")]
+		public string Image { get; set; }
+
+		public string Description { get; set; }
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+
+		public override string ToString() => JsonSerializer.Serialize<Athlete>(this);
+        
+    }
+}
+
