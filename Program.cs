b0cfd91e80430707cd14018234a877f3543856a3
Ralph Etienne <ralphyetienne@yahoo.com>
2023-02-22 10:59:04 -0500

    Added my AthleteService to the program.cs so I should be able to retreive some info

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BioFan.Services;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<AthleteService>();
 
 var app = builder.Build();
 
---
+++ Services/AthleteService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using BioFan.Models;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace BioFan.Services
+{
+	public class AthleteService
+	{
+        public AthleteService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        } 
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "athletes.json"); }
+        }
+
+
+        public IEnumerable<Athlete> GetAthletes()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Athlete[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
+
